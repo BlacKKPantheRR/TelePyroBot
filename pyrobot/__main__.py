3255f7d1c6af5187557d127f230271e4d64eb50b
Skuzzy xD <techdroidroot@gmail.com>
2020-06-06 17:21:33 +0530

    USE Loop

--- pyrobot/__main__.py
+++ pyrobot/__main__.py
@@ -1,8 +1,11 @@
 from pyrobot import PyroBotCMD
+import asyncio
+
+loop = asyncio.get_event_loop()
 
 async def start_bot():
     await PyroBotCMD.start()
 
 if __name__ == '__main__':
     LOGGER.info(f"TelePyroBot based on Pyrogram started Successfully! Hello User.")
-    start_bot()
+    loop.run_until_complete(start_bot())
